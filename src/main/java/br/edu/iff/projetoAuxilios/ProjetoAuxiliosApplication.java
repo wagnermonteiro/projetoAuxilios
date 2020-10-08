e2a6dd4bfe5a4fdb95c5726d32790cf7317240c3
Wagner <Wagner@DESKTOP-5LSHH6H>
2020-10-08 19:07:15 -0300

    Criação do projeto.

---
+++ src/main/java/br/edu/iff/projetoAuxilios/ProjetoAuxiliosApplication.java
@@ -0,0 +1,13 @@
+package br.edu.iff.projetoAuxilios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAuxiliosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAuxiliosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/iff/projetoAuxilios/ProjetoAuxiliosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iff.projetoAuxilios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoAuxiliosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
